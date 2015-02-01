71485d05d27713df40d818d7a2a25fee793a4820
yussan <yussandeveloper@gmail.com>
2015-02-01 18:05:34 +0700

    yussan localhost db

--- application/config/database.php
+++ application/config/database.php
@@ -50,8 +50,8 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '123';
-$db['default']['database'] = 'db_toko_ukm';
+$db['default']['password'] = '';
+$db['default']['database'] = '2015_alvinukmbantul';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
